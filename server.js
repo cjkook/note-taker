f15854ea3d5b1b3678787697612d3ea5e6847ac7
cjkook <cjko.audio@gmail.com>
2020-07-24 11:22:47 -0600

    intialize, routes

---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname,"public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// read all saved notes
+app.get("/api/notes", function(req, res) {
+  return res.json(characters);
+});
+
+// add note
+app.post("/api/notes", function(req, res) {
+  // ! encapsulate note into an object/class
+  let newNote = req.body
+  
+  console.log(newNote);
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newNote.noteId = 123 // ! random number generation for id? Needs to check old ids for duplicates
+
+  
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
