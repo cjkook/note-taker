9a64944b4f20fddb29dda4ba16cc350fee892f1f
cjkook <cjko.audio@gmail.com>
2020-08-21 09:04:25 -0600

    port fix

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var fs = require("fs")
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
